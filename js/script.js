e154f38855d4084552ee1c04b76d492fc5b467b4
ElaineTavares <etsets.tavares@gmail.com>
2024-01-24 07:09:55 -0300

    sound

--- js/script.js
+++ js/script.js
@@ -6,14 +6,17 @@ function insert(num){
 
 function clean(){
     document.getElementById("resultado").innerHTML = "";
+    document.querySelector("#som").play();
 }
 
 function back(){
     var resultado = document.getElementById("resultado").innerHTML;
     document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length -1);
+    document.querySelector("#som").play();
 }
 
 function calcular(){
+    document.querySelector("#som").play();
     var resultado = document.getElementById("resultado").innerHTML;
     if(resultado){
         document.getElementById("resultado").innerHTML = eval(resultado);
